cee8df6e1ca4200870f412d728cdd8ad00be74c7
Pavel Vasiliev <lyftzeigen@mail.ru>
2022-03-22 12:43:06 +0300

    Revert "Bugfix with input in Launcher TextBoxes"

--- Launcher/Settings.cs
+++ Launcher/Settings.cs
@@ -10,19 +10,19 @@
         public static int FieldWidth
         {
             get { return fieldWidth; }
-            set { fieldWidth = value; }
+            set { fieldWidth = value < 25 ? 25 : value; }
         }
 
         public static int FieldHeight
         {
             get { return fieldHeight; }
-            set { fieldHeight = value; }
+            set { fieldHeight = value < 25 ? 25 : value; }
         }
 
         public static int BlokSize
         {
             get { return blokSize; }
-            set { blokSize = value; }
+            set { blokSize = value < 5 ? 5 : value; }
         }
 
         public static int TerrainDomainNumber { get; set; }
